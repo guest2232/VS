c0efb54964fde598c14b5fdb54d79a4c5c780ea4
Muhammad Zaid <guyspakistan@gmail.com>
2024-10-09 20:09:22 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+a={2,5,4,7,7,5,21,8,8,64}
+print("integer set:", a)
+b={23, 'hello', 3, 2, False}
+print('mixed Datatype set:', b)
+c={1,2,3}
+print('upgraded integer:', c)
+c.add(12)
+print('upgraded integer:', c)
